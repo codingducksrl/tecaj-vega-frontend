352a603dce903f5bc084f9bd21c969b9516a24b9
Guglielmo Frati <guglielmo.frati@codingduck.it>
2025-05-14 10:00:52 +0200

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Dokumentacija
+
+### Catalyst
+
+https://catalyst.tailwindui.com/docs
+
+### React
+
+https://react.dev/learn
+
+### Tailwind
+
+https://tailwindcss.com/docs/installation/using-vite
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Heading} from "./components/catalyst/heading.tsx";
+
+function App() {
+    return (
+        <>
+            <div
+                className="flex w-full flex-wrap items-end justify-between gap-4 border-b border-zinc-950/10 pb-6 dark:border-white/10">
+                <Heading>Hello world</Heading>
+            </div>
+        </>
+    )
+}
+
+export default App
