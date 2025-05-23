81a03e5f6a09b9e5b7a523dbf29983841c1f7826
Vip3r00 <andrea.silvestri@potep.org>
2025-05-23 14:01:03 +0200

    submit login URL value update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,24 +1,25 @@
-import { Avatar } from '@/components/catalyst/avatar.tsx'
+import { Button } from "@/components/catalyst/button";
+import { Input } from "@/components/catalyst/input";
+import { Heading } from "@components/catalyst/heading.tsx";
+import { useState } from "react";
 
-export default function Login() {
+export default function Home() {
+
+    var [id, setId] = useState("")
+
     return (
         <>
-            <div
-                className="flex w-full flex-wrap items-end justify-between gap-4 border-b border-zinc-950/10 pb-6 dark:border-white/10">
-                <h1>world</h1>
-                
+            <div className="flex flex-col w-full items-center">
+                <div className="w-[350px] mt-[100px]">
+                    <Heading>Wassup gamer!</Heading>
+                    
+                    <div className="mt-[20px] flex flex-col gap-y-3">
+                        <Input placeholder="i will not steal your data..  just your [SteamID]" onChange={(event)=>{setId(event.target.value)}}></Input>
+                        {id}
+                        <Button href={`/page?steamid=${id}`} color='light' >Request your new game</Button>
+                    </div>
+                </div>
             </div>
-            <Example></Example>
-
         </>
     )
-}
-function Example() {
-  return (
-    <>
-      <Avatar initials="tw" className="size-6 bg-zinc-400 text-white dark:bg-blue dark:text-black" />
-      <Avatar initials="tw" className="size-8 bg-zinc-900 text-white dark:bg-white dark:text-black" />
-      <Avatar initials="tw" className="size-10 bg-zinc-900 text-white dark:bg-white dark:text-black" />
-    </>
-  )
 }
