c8f6ba9d02c9f95d4952ebce952e441c2af3e855
caso-cyber <mattia.caso@potep.org>
2025-06-03 11:04:33 +0200

    some fixes wih conflicts

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,12 @@
 import {Route, Routes} from "react-router";
 import Login from "@/pages/Login";
-import Homepage from "@/pages/Homepage";
+import HomePage from "@/pages/HomePage";
 
 function Router() {
     return (
         <Routes>
             <Route path="/" element={<Login/>}/>
-            <Route path="/home" element={<Homepage/>}/>
+            <Route path="/home" element={<HomePage/>}/>
         </Routes>
     )
 }
