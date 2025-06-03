766166fcf71bf42e65ab19b70862e0ed197c362d
caso-cyber <mattia.caso@potep.org>
2025-06-03 10:51:38 +0200

    Filter updated SAMO ime+slika

--- src/components/atoms/Slider.tsx
+++ src/components/atoms/Slider.tsx
@@ -1,29 +1,49 @@
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 
-const user = {
-  name: 'Marvel Rivals',
-  color: 'chartreuse',
-};
-
-export default function Slider(props: {games: {id: string,name: string, descr:string, image:string}[]}) {
+export default function Slider(props: 
+    {games: 
+        {id: string,name: string, descr:string, category:string, image:string}[]}) {
     return (
         <>
             <div className='w-screen' >
                 <Slide slidesToScroll={1} slidesToShow={4} indicators={true} arrows={true} infinite={true} transitionDuration={300}>
                     {
                         props.games.map((game)=>{
-                            return <div style={{
-                                textAlign: 'center',
-                                backgroundColor: 'red',
-                                backgroundImage: `url(${game.image})`,
-                                padding: '100px 0',
-                                fontSize: '30px',
+                        return (
+                        <div
+                            style={{
+                            position: 'relative',
+                            backgroundImage: `url(${game.image})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            marginRight: '15px',
+                            height: '245px', // adjust height as needed
+                            }}
+                        >
+                            <div
+                            style={{
+                                position: 'absolute',
+                                bottom: 0,
+                                width: '100%',
                                 
-                            }}><a href='https://store.steampowered.com/app/2767030/Marvel_Rivals/'>{game.name + game.descr}</a></div>
-                        })
-                    }
-                    
+                                backgroundColor: 'black',
+                                textAlign: 'center',
+                                padding: '6px',
+                                fontSize: '23px',
+                            }}
+                            >
+                            <a
+                                href="https://store.steampowered.com/app/2767030/Marvel_Rivals/"
+                                style={{ color: 'white'}}
+                            >
+                                {game.name}
+                            </a>
+                            </div>
+                        </div>
+                        );})
+                                            }
+                                            
                 </Slide>
             </div>
         </>
